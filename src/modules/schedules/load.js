5fd3aa6c71dbfce239493b1a96944df03db4fe76
Victor Martins Dias <victormartinsjob@gmail.com>
2026-08-27 13:29:48 -0300

    feat(schedules): render daily appointments dynamically in HTML periods

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,15 @@
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { schedulesShow } from "../schedules/show.js";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value;
+
+  const dailySchedules = await scheduleFetchByDay({ date });
+
+  schedulesShow({ dailySchedules });
 
   hoursLoad({ date });
 }
