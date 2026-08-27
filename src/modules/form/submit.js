340685bf2013fcd59182b2586d0de3bb407ab997
Victor Martins Dias <victormartinsjob@gmail.com>
2026-08-26 21:38:00 -0300

    feat(form): integrate scheduleNew service into form submission

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 
+import { scheduleNew } from "../../services/schedule-new.js";
+
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
@@ -8,7 +10,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 selectedDate.value = inputToday;
 selectedDate.min = selectedDate.value = inputToday;
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault();
 
   try {
@@ -23,6 +25,18 @@ form.onsubmit = (event) => {
     if (!hourSelected) {
       return alert("Selecione a hora.");
     }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = String(new Date().getTime());
+
+    await scheduleNew({
+      id,
+      name,
+      when,
+    });
   } catch (error) {
     alert("Não foi possível realizar o agendamento.");
     console.error(error);
